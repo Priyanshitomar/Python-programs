343de51f8561ad49a5bcc79907342b27b250d976
Priyanshi Tomar <168635744+Priyanshitomar@users.noreply.github.com>
2025-01-04 14:08:48 +0530

    Add files via upload

---
+++ Binary to Decimal Conversion.py
@@ -0,0 +1,9 @@
+binary = input("Enter a binary number: ")
+decimal = int(binary, 2)
+print("Decimal:", decimal)
+
+# 16. Print even numbers from 0 to 10
+for i in range(11):
+    if i % 2 == 0:
+        print(i, end=" ")
+print()
