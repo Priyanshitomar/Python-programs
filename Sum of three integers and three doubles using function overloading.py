d0caed13a5bb70e4928c09b8ab71c01866d1f224
Priyanshi Tomar <168635744+Priyanshitomar@users.noreply.github.com>
2025-01-25 17:28:47 +0530

    Create Sum of three integers and three doubles using function overloading.py

---
+++ Sum of three integers and three doubles using function overloading.py
@@ -0,0 +1,8 @@
+def calculate_sum(*args):
+    return sum(args)
+
+# Three integers
+print("Sum of integers:", calculate_sum(1, 2, 3))
+
+# Three doubles
+print("Sum of doubles:", calculate_sum(1.1, 2.2, 3.3))
