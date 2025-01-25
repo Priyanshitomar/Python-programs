3d0c342a2aa30bcfe25cd783b635e05694288472
Priyanshi Tomar <168635744+Priyanshitomar@users.noreply.github.com>
2025-01-25 17:33:54 +0530

    Create Display roll number and name using constructor.py

---
+++ Display roll number and name using constructor.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, roll_no, name):
+        self.roll_no = roll_no
+        self.name = name
+
+    def display(self):
+        print(f"Roll No: {self.roll_no}, Name: {self.name}")
